a2ebb83f6557e70e419f30c2654645683a12d6de
Florent BELLENGER <bellengerflorent@gmail.com>
2020-05-13 18:11:34 +0200

    creation des fichiers indy1.php et indy.php

---
+++ indy1.php
@@ -0,0 +1,32 @@
+<?php
+
+$movies = [
+    "Indiana Jones and the Kingdom of the Crystal Skull"    => [
+        "Harrison Ford",
+        "Cate Blanchett",
+        "Karen Allen"
+    ],
+    "Indiana Jones and the Last Crusade"    => [
+        "Harrison Ford",
+        "Sean Connery",
+        "Denholm Elliott"
+    ],
+    "Indiana Jones and the Temple of Doom"  => [
+        "Harrison Ford",
+        "Kate Capshaw",
+        "Jonathan Ke Quan"
+    ],
+];
+foreach ($movies as $movie => $names) {
+    echo "Dans le film " . $movie . ", les principaux acteurs sont : ";
+    $count = 0;
+    foreach($names as $name) {
+        $count ++;
+        if ($count < 3) {
+            echo $name . ", ";
+        } else {
+            echo $name . ".";
+        }
+    }
+    echo "<br>" . PHP_EOL;
+}
